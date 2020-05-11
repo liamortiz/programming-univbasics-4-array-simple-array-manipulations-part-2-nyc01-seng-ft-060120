f14b0f06bb030441473306ac4c80f47055eed48c
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 15:52:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,8 @@ def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
 
-def using_insert(arr1, element)
-  
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
